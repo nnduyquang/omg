3de8f0e8582f4a32557471834a4bdacad8af9364
nnduyquang@gmail.com <bunvodich>
2019-06-20 11:05:53 +0700

    BIG UPDATE

--- local/webpack.mix.js
+++ local/webpack.mix.js
@@ -15,5 +15,7 @@ mix.setResourceRoot('../');
 
 mix.js('resources/assets/js/app.js', 'js/app.js')
     .styles('resources/assets/js/backend.js', '../js/backend.js')
+    .styles('resources/assets/js/core.common.js', '../js/core.common.js')
     .sass('resources/assets/sass/app.scss', 'css/app.css')
-    .sass('resources/assets/sass/backend/backend.scss','css/backend.css').options({processCssUrls: false});
+    .sass('resources/assets/sass/backend/backend.scss','css/backend.css').options({processCssUrls: false})
+    .sass('resources/assets/sass/frontend/common/common.scss', '../css/common.css').options({processCssUrls: false})
